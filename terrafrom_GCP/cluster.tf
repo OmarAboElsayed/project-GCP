787d6fcb3981797e675ec6828c3ae44dceb0d72f
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-26 04:21:05 +0200

    Update cluster.tf

--- terrafrom_GCP/cluster.tf
+++ terrafrom_GCP/cluster.tf
@@ -32,19 +32,3 @@ resource "google_container_cluster" "my-cluster" {
 
 }
 
-resource "google_container_node_pool" "nodepool" {
-  name       = "nodepool"
-  location   = "us-central1-a"
-  cluster    = google_container_cluster.my-cluster.id
-  node_count = 1
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-    # custom service account
-    service_account = google_service_account.node-service-account.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
