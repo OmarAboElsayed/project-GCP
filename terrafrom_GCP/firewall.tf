0283e1bf4e6e61825e04d243855bf2abbfe7763f
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-26 17:52:07 +0200

    Update firewall.tf

--- terrafrom_GCP/firewall.tf
+++ terrafrom_GCP/firewall.tf
@@ -1,15 +1,6 @@
-# resource "google_compute_firewall" "allow-iap" {
-#   name    = "allow-iap"
-#   network = google_compute_network.myvpc.id
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80", "22"]
-#   }
-#   direction     = "INGRESS"
-#   source_ranges = ["35.235.240.0/20"]
-# }
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "allow-ssh"
+
+resource "google_compute_firewall" "iap" {
+  name    = "iap"
   network = google_compute_network.myvpc.name
 
   allow {
