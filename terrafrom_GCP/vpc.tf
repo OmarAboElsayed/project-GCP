d3d12f3518895fb883b15c8e10ac2fe1a9cdd96c
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-26 17:53:54 +0200

    Update vpc.tf

--- terrafrom_GCP/vpc.tf
+++ terrafrom_GCP/vpc.tf
@@ -2,5 +2,5 @@ resource "google_compute_network" "myvpc" {
   project                 = "omar-mohamed-el-sayed-project"
   name                    = "myvpc"
   auto_create_subnetworks = false
-  mtu                     = 1460
+ 
 }
