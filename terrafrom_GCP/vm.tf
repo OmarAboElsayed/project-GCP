f42cda00d0f56359ea2f0ed2cb88aab30f5d0b79
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-26 17:39:06 +0200

    Update vm.tf

--- terrafrom_GCP/vm.tf
+++ terrafrom_GCP/vm.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "vm" {
       mkdir -p ~/.local/bin
       mv ./kubectl ~/.local/bin/kubectl
       kubectl version --client
-      [4:41 PM]
+     
       sudo apt-get install  -y apt-transport-https ca-certificates gnupg
       echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
       curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
