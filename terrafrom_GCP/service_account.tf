af85974cb22b1b5fce10cdee5ef4a59040445cfb
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-26 18:55:19 +0200

    Update service_account.tf

--- terrafrom_GCP/service_account.tf
+++ terrafrom_GCP/service_account.tf
@@ -2,7 +2,6 @@ resource "google_service_account" "node-service-account" {
   account_id = "node-service-account"
   project = "omar-mohamed-el-sayed-project"
 }
-
 resource "google_project_iam_binding" "node-service-account-iam" {
   project = "omar-mohamed-el-sayed-project"
   role    = "roles/storage.admin"
@@ -11,4 +10,15 @@ resource "google_project_iam_binding" "node-service-account-iam" {
   ]
 }
 
+resource "google_service_account" "k8s-sa" {
+  account_id   = "k8s-sa"
+  display_name = "k8s-sa"
+}
 
+resource "google_project_iam_binding" "k8s-sa-binding" {
+  project = omar-mohamed-el-sayed-project
+  role    = "roles/storage.admin" # Full access to storage
+  members = [
+    "serviceAccount:${google_service_account.k8s-sa.email}"
+  ]
+}
