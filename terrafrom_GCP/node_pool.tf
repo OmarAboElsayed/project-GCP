59c7ce9a53e71de40337772cde790bf5b5b79d0c
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-27 23:38:01 +0200

    Update node_pool.tf

--- terrafrom_GCP/node_pool.tf
+++ terrafrom_GCP/node_pool.tf
@@ -4,13 +4,59 @@ resource "google_container_node_pool" "nodepool" {
   cluster    = google_container_cluster.my-cluster.id
   node_count = 1
 
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
   node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-    # custom service account
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      role = "general"
+    }
+
     service_account = google_service_account.node-service-account.email
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
+
+resource "google_container_node_pool" "spot" {
+  name    = "spot"
+  cluster = google_container_cluster.my-cluster.id
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-small"
+
+    labels = {
+      role = "general"
+    }
+
+    taint {
+      key    = "instance_type"
+      value  = "spot"
+      effect = "NO_SCHEDULE"
+    }
+
+    service_account =  google_service_account.node-service-account.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+
