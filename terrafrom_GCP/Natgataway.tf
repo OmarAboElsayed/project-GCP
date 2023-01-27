46fca69016bdfa2408c4856c160c7212bfb6416d
Omar Mohamed Mohamed ElSayed Hassan <92756055+OmarAboElsayed@users.noreply.github.com>
2023-01-27 23:45:08 +0200

    Update Natgataway.tf

--- terrafrom_GCP/Natgataway.tf
+++ terrafrom_GCP/Natgataway.tf
@@ -1,11 +1,23 @@
-resource "google_compute_router_nat" "nat-gw" {
-  name                               = "nat"
-  router                             = google_compute_router.router.name
-  region                             = google_compute_router.router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  router = google_compute_router.router.name
+  region = "us-central1"
+
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
   subnetwork {
-    name                    = google_compute_subnetwork.management.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    name = google_compute_subnetwork.management.id
+    source_ip_ranges_to_nat = ["10.0.1.0/24"]
   }
+
+  nat_ips = [google_compute_address.nat.self_link]
 }
+
+
+resource "google_compute_address" "nat" {
+  name         = "nat"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+
+  depends_on = [google_project_service.compute]
